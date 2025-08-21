1be5a05db6cdbae833ee12360f364f39d70350a7
JuanC <jcpallaresvalenzuela@gmail.com>
2025-08-21 15:41:03 +0200

    Add README.md

--- terraform/acr-tasks/main.tf
+++ terraform/acr-tasks/main.tf
@@ -2,5 +2,5 @@ module "acr_task_delete_repos" {
   source              = "../modules/acr-task-delete-repos"
   resource_group_name = var.resource_group_name
   acr_name            = var.acr_name
-  schedule_cron       = "0 1 * * *"  # 01:00 UTC (~03:00 Madrid en verano)
+  schedule_cron       = var.schedule_cron  # UTC
 }
